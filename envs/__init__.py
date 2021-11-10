aae1c9977a2267652182ee1b08c10c11fbc4f8bc
dwjshift <gdwjshift@gmail.com>
2021-11-10 09:00:37 +0000

    envs

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,9 +1,11 @@
 from envs.predatorprey_wrapper import PredatorPreyWrapper
 from envs.smac_wrapper import SMACWrapper
 from envs.traffic_junction_wrapper import TrafficJunctionWrapper
+from .aloha import AlohaEnv
 
 __all__ = [
     'PredatorPreyWrapper',
     'SMACWrapper',
     'TrafficJunctionWrapper',
+    'AlohaEnv',
 ]
