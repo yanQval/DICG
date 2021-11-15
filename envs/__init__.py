73074b93625fa07a4adc9382058d88a13028bf1a
yanQval <yanqval@gmail.com>
2021-11-15 23:06:21 +0800

    fix bugs

--- envs/__init__.py
+++ envs/__init__.py
@@ -2,10 +2,12 @@ from envs.predatorprey_wrapper import PredatorPreyWrapper
 from envs.smac_wrapper import SMACWrapper
 from envs.traffic_junction_wrapper import TrafficJunctionWrapper
 from envs.aloha_wrapper import AlohaWrapper
+from envs.disperse_wrapper import DisperseWrapper
 
 __all__ = [
     'PredatorPreyWrapper',
     'SMACWrapper',
     'TrafficJunctionWrapper',
     'AlohaWrapper',
+    'DisperseWrapper'
 ]
